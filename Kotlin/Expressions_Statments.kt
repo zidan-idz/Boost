6931a2f7daa767b328e8857688085c18e995a81c
Muhammad Raid Zaidani <60657412+zidan-idz@users.noreply.github.com>
2024-10-30 23:17:28 +0800

    Create Expressions_Statments.kt

---
+++ Kotlin/Expressions_Statments.kt
@@ -0,0 +1,23 @@
+
+ fun A(){
+     val now = 11
+     val fin = 12
+     if(now > fin) {
+         println("$now lebih besar dari $fin")
+     } else {
+         println("$now tidak lebih besar dari $fin")
+     }
+
+ }
+
+ fun D(){
+     sum(1, 2 * 3)
+ }
+
+ fun D(){
+     val index1 = 2
+     val index2 = 4
+     sum(index1 , index2)
+ }
+
+ fun sum(index1: Int, index2: Int)= index1 + index2
